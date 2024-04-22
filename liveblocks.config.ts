13f97f6abb03b8ac8cfaebf959f9104e59c0306d
Salih Yılmaz <salih_yil@live.com>
2024-04-22 17:45:58 +0300

    Room Authorization

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createLiveblocksContext, createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey: "pk_dev_-QB6IQyWWYeV5GXbZCA1C6J8Vd5V98arRfR3Hg7jJsejqoZ5zbKSQQ_6U-hGuJjw",
-  // authEndpoint: "/api/liveblocks-auth",
+  authEndpoint: "/api/liveblocks-auth",
   // throttle: 100,
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
@@ -68,8 +67,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  };
 };
 
 // Optionally, the type of custom events broadcast and listened to in this

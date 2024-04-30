9c0da603f5b99a064939d3dfd7fa030c0f8ef865
Salih Yılmaz <salih_yil@live.com>
2024-04-30 17:49:15 +0300

    process.env.LIVEBLOCKS_SECRET_KEY

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -8,7 +8,7 @@ import { api } from "../../../../convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret: "sk_dev_kdzZNwf-JyWuMtJ6IzQTK5EMkKjEEC7-7cEhynwN0XkHGUA-Lt9aAngAQ1hcygap",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
